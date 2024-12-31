d12fbf95df0c5c1b16cd93e33db2e397b1a2ee2d
RandithaK <bharkula3@gmail.com>
2025-01-01 02:33:42 +0530

    Completed pages for email verification, OTP and password reset

---
+++ app/Enteremail.tsx
@@ -0,0 +1,138 @@
+import React from "react";
+import {
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+interface Props {
+  navigation: NativeStackNavigationProp<any>;
+}
+
+interface ButtonProps {
+  title: string;
+  onPress?: () => void;
+  style?: object;
+}
+
+const CustomButton: React.FC<ButtonProps> = ({ title, onPress, style }) => (
+  <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
+    <Text style={styles.buttonText}>{title}</Text>
+  </TouchableOpacity>
+);
+
+const App: React.FC<Props> = ({ navigation }) => {
+  const backgroundImage: ImageSourcePropType = {
+    uri: "https://www.annees-de-pelerinage.com/wp-content/uploads/2019/03/elephants.jpg",
+  };
+  const logoImage: ImageSourcePropType = {
+    uri: "https://www.annees-de-pelerinage.com/wp-content/uploads/2019/03/elephants.jpg",
+  };
+
+  return (
+    <ImageBackground source={backgroundImage} style={styles.background}>
+      <StatusBar style="light" />
+      <View style={styles.overlay}>
+        <View
+          style={[
+            styles.semiTransparentBlock,
+            {
+              flex: 0.75, // Takes up 50% of the parent height
+              marginVertical: "25%", // Pushes the block to the middle
+            },
+          ]}
+        >
+          <View style={styles.contentContainer}>
+            <TextInput
+              style={[styles.input, styles.marginBottom]}
+              placeholder="Enter your email"
+              placeholderTextColor="#4F46E5"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+            <View style={{ height: 50 }} />
+            <CustomButton title="Continue" />
+          </View>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
+  },
+  semiTransparentBlock: {
+    flex: 1,
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+  },
+  logoContainer: {
+    alignItems: "center",
+    marginTop: 40,
+  },
+  logoWrapper: {
+    backgroundColor: "rgb(186, 230, 253)", // sky-200 equivalent
+    borderRadius: 50,
+    padding: 16,
+    width: 96,
+    height: 96,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 64,
+    height: 64,
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 32,
+  },
+  titleText: {
+    color: "white",
+    fontSize: 36,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 80,
+  },
+  button: {
+    backgroundColor: "rgba(234, 88, 12, 1)",
+    borderRadius: 8,
+    paddingVertical: 16,
+    fontSize: 20,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#000000",
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  input: {
+    height: 40,
+    backgroundColor: "rgba(207, 231, 229, 0.8)",
+    borderColor: "rgba(207, 231, 229, 0.8)",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    color: "white",
+    fontSize: 20,
+  },
+  marginBottom: {
+    marginBottom: 16,
+  },
+});
+
+export default App;
