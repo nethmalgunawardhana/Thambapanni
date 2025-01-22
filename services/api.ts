00b7ab667d04ff07adbe50d723532ed0c57a1e66
AdithaBuwaneka <adithabuwaneka0@gmail.com>
2025-01-22 10:32:54 +0530

    uri updated

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { getAuthToken } from './auth/loginService'
-export const API_URL = 'http://192.168.246.83:5000'; // Replace with your backend URL
+export const API_URL = 'https://thambapanni-backend.vercel.app'; // Replace with your backend URL
 
 export const apiClient = axios.create({
   baseURL: API_URL,
